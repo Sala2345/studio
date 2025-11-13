20eca9b866e5a47efea3d3bd6ab5b193abf6db00
David Aribisala <david@craniumx.ca>
2025-11-13 16:19:59 +0000

    Try fixing this error: `Runtime Error: Invalid src prop (https://cdn.sho

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
@@ -31,6 +32,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'cdn.shopify.com',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
 };
