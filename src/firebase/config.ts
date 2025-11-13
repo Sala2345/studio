6e02d9a7bc05953a67b88baf58500229e6db3925
David Aribisala <david@craniumx.ca>
2025-11-13 14:16:10 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6281389376-e03d5",
+  "appId": "1:921969095503:web:9e1fd5e52d6083cd8a8b3a",
+  "apiKey": "AIzaSyDeig0FpEoTwu-03HCt_J7jnWJLdfHRRTE",
+  "authDomain": "studio-6281389376-e03d5.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "921969095503"
+};
