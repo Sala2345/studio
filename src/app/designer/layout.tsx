1f76705408fd1090ae457414d3746020bdcfdb6a
David Aribisala <david@craniumx.ca>
2025-11-13 13:51:05 +0000

    Try fixing this error: `Console Error: A tree hydrated but some attribut

--- src/app/designer/layout.tsx
+++ src/app/designer/layout.tsx
@@ -12,15 +12,8 @@ export default function DesignerLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
-      </head>
-      <body className="font-body antialiased bg-background">
+    <div className="bg-background">
         {children}
-      </body>
-    </html>
+    </div>
   );
 }
