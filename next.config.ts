6a8e330fcd2bfb94da1323ab93d6e0c2841e74c1
David Aribisala <david@craniumx.ca>
2025-11-14 13:38:33 +0000

    ⚠ Invalid next.config.ts options detected:  ⚠     Unrecognized key(s) i

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,7 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  serverActions: {
+  experimental: {
     bodySizeLimit: '10mb',
   },
   typescript: {
